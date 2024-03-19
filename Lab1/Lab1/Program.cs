84eaf7e68afd90ce8136c98b1e4f4d35635f47ba
timajan07 <timajan07@gmail.com>
2024-03-19 22:33:47 +0200

    initialize

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //#1
+        //Вивести вміст списку по 10 елементів в рядку так, щоб висновок відбувався по спіралі, тобто кожен другий рядок повинен виводитись задом наперед.
+        List<int> list = new List<int>();
+        for (int i = 1; i <= 100; i++)
+        {
+            list.Add(i);
+        }
+        int row = 10;
+        int count = 0;
+        bool reverse = false;
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i != 0 && (decimal)i / row % 1 == 0)
+            {
+                reverse = !reverse;
+                count++;
+                Console.WriteLine();
+            }
+            if (!reverse)
+            {
+                Console.Write(list[i] + " ");
+            }
+            else
+            {
+                int index = 2 * (row * count) - i - 1 + row;
+                Console.Write(list[index] + " ");
+            }
+        }
+        Console.ReadKey();
+        Console.Clear();
+
+        //#2
+        //Дано словник з масивами літер у значеннях ключа. Вивести на екран комбінації усіх літер, перебравши всі можливі комбінації літер різних ключів словника.
+        //Вхідний словник : {'1':['A','d'], '2':['C','B']}. Вихідний результат ACAB dCdB
+
+        var dict = new Dictionary<int, List<char>>()
+        {
+            { 1, new List<char> {'A', 'd'} },
+            { 2, new List<char> {'C', 'B'} }
+        };
+
+        List<string> combinations = new List<string>();
+        GenerateCombinations(dict, "", 1, combinations);
+
+        foreach (string value in combinations)
+        {
+            Console.Write(value + " ");
+        }
+
+        Console.ReadKey();
+        Console.Clear();
+
+        //#3
+        //Дана послідовність непустих рядків A. Отримати послідовність символів, кожен елемент якої є початковим символом відповідного рядка з A.
+        //Порядок символів повинен бути зворотним до порядку елементів вихідної послідовності. (3)
+
+        List<string> strings = new List<string>()
+        {
+            "Hello",
+            "my",
+            "name",
+            "is",
+            "Tim"
+        };
+
+        var symbols = strings.Select(x => x[0]).Reverse();
+        
+        foreach (var x in symbols)
+        {
+            Console.Write(x + " ");
+        }
+        Console.ReadKey();
+    }
+
+    public static void GenerateCombinations(Dictionary<int, List<char>> dictionary, string current, int index, List<string> combinations)
+    {
+        if (index > dictionary.Count)
+        {
+            combinations.Add(current);
+            return;
+        }
+
+        foreach (char c in dictionary[index])
+        {
+            GenerateCombinations(dictionary, current + c, index + 1, combinations);
+        }
+    }
+}
---
+++ Lab3/Lab3/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SequenceAnalysis
+{
+    public class Sequence
+    {
+        public List<int> Numbers { get; set; }
+
+        public Sequence(List<int> numbers)
+        {
+            Numbers = numbers;
+        }
+
+        // Checks if the sequence is strictly increasing
+        public static bool IsIncreasing(List<int> sequence)
+        {
+            for (int i = 1; i < sequence.Count; i++)
+            {
+                if (sequence[i] <= sequence[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Checks if the sequence is strictly decreasing
+        public static bool IsDescending(List<int> sequence)
+        {
+            for (int i = 1; i < sequence.Count; i++)
+            {
+                if (sequence[i] >= sequence[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Checks if the sequence is non-increasing (monotonically decreasing)
+        public static bool IsNonIncreasing(List<int> sequence)
+        {
+            for (int i = 1; i < sequence.Count; i++)
+            {
+                if (sequence[i] > sequence[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Checks if the sequence is an arithmetic progression
+        public static bool IsArithmeticProgression(List<int> sequence)
+        {
+            if (sequence.Count < 2) return false;
+
+            int difference = sequence[1] - sequence[0];
+            for (int i = 2; i < sequence.Count; i++)
+            {
+                if (sequence[i] - sequence[i - 1] != difference)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Checks if the sequence is a geometric progression
+        public static bool IsGeometricProgression(List<int> sequence)
+        {
+            if (sequence.Count < 2 || sequence.Contains(0)) return false;
+
+            double ratio = (double)sequence[1] / sequence[0];
+            for (int i = 2; i < sequence.Count; i++)
+            {
+                if ((double)sequence[i] / sequence[i - 1] != ratio)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        // Deserialization method
+        public static Sequence LoadFromJson(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Sequence>(json);
+        }
+
+    }
+}
